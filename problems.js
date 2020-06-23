046f62124fc7a6870ef4e3ce90d6364456713049
justincheng20 <justincheng20@gmail.com>
2020-06-22 22:15:30 -0700

    length of last word

--- problems.js
+++ problems.js
@@ -129,3 +129,8 @@ function formatString(myString, maxRepeat){
   }
   return newWord;
 };
+
+var lengthOfLastWord = function(s) {
+    let words = s.trim().split(" ");
+    return words[words.length - 1].length;
+};
