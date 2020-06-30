6a950bbf8839089fb53e601abd41b09a2519e7a9
justincheng20 <justincheng20@gmail.com>
2020-06-29 21:10:13 -0700

    unique paths

--- problems.js
+++ problems.js
@@ -134,3 +134,30 @@ var lengthOfLastWord = function(s) {
     let words = s.trim().split(" ");
     return words[words.length - 1].length;
 };
+
+var uniquePaths = function(m, n) {
+  let arr = [];
+  for (let i = 0; i < m; i++){
+      let row = []
+      for (let j = 0; j < n; j++){
+          row.push(0);
+      }
+      arr.push(row);
+  }
+
+  for (let i = 0; i < m; i++){
+      arr[i][0] = 1;
+  }
+  for (let i = 0; i < n; i++){
+      arr[0][i] = 1;
+  }
+  arr[0][0] = 1;
+  for (let i = 1; i < m; i++){
+      for (let j = 1; j < n; j++){
+      let right = arr[i-1]
+          arr[i][j] = arr[i][j-1] + arr[i-1][j];
+          
+      }
+  }
+  return arr[m - 1][n - 1];
+};
