3dc04b2edea90f8759614b092e1ef151fae8cce9
justincheng20 <justincheng20@gmail.com>
2020-07-24 21:04:54 -0700

    buy and sell stock 2

--- problems.js
+++ problems.js
@@ -234,3 +234,17 @@ var maxProfit = function(prices) {
   }
   return max;
 };
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let profit = 0;
+  for (let i = 0; i < prices.length - 1; i++){
+      if (prices[i + 1] > prices[i]){
+          profit += prices[i+1] - prices[i];
+      }
+  }
+  return profit;
+};
