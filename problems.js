2aa6c303cee21eccedff7692199f1463f49df7bd
justincheng20 <justincheng20@gmail.com>
2020-08-17 21:55:25 -0700

    delete node

--- problems.js
+++ problems.js
@@ -495,3 +495,19 @@ var countSegments = function(s) {
   let output = arr.filter(word => word.trim().length !== 0);
   return output.length;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+  node.val = node.next.val;
+  node.next = node.next.next;
+};
