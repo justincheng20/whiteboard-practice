dc8b3f6c489a83672b0a38ea247fbb0a0319e7c7
justincheng20 <justincheng20@gmail.com>
2020-08-26 22:13:34 -0700

    invert binary tree

--- problems.js
+++ problems.js
@@ -580,3 +580,31 @@ var isPrime= function(num) {
    }
    return true;
 }
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if (root === null) return null;
+  
+  function helper(node){
+      if (node === null) return;
+      let temp = node.right;
+      node.right = node.left;
+      node.left = temp;
+      helper(node.left);
+      helper(node.right);
+      return node;
+  }
+  
+  return helper(root)
+};
